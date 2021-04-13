4c14d0cae61a8ff7c135f8c21e51c6e9613523e1
Quintin Henn <quintin.henn@gmail.com>
2021-04-13 17:27:42 +0200

    PyGame Zero with Mu Editor

--- introduction-to-pygame-zero/README.md
+++ introduction-to-pygame-zero/README.md
@@ -4,9 +4,32 @@
 
 ## :bookmark_tabs: Running the Code
 
+### Step 1: Install Virtual Environment
+
 ```shell
 pipenv shell
 pipenv install pgzero
-pgzrun intro.py
+pgzrun introduction/intro.py
+exit
+```
+
+### Step 2: Intstall and Run PyGmae Zero
+
+```shell
+pipenv install pgzero
+pgzrun introduction/intro.py
+```
+
+### Step 3: Install and Run Mu Editor
+
+```shell
+pipenv install mu-editor --dev
+mu-editor
+```
+
+### Step 4: Finalizing the Project Dependencies
+
+```shell
+pipenv lock
 exit
 ```
--- introduction-to-pygame-zero/introduction/intro.py
+++ introduction-to-pygame-zero/introduction/intro.py
@@ -1,9 +1,10 @@
+import pgzrun
+
 alien = Actor('alien')
 alien.topright = 0, 10
 
 WIDTH = 500
 HEIGHT = alien.height + 20
-
 
 def draw():
     screen.clear()
@@ -16,19 +17,21 @@ def update():
         alien.right = 0
 
 
-def on_mouse_down(pos):
-    if alien.collidepoint(pos):
-        set_alien_hurt()
+def on_mouse_down(pos, button):
+    if button == mouse.LEFT and alien.collidepoint(pos):
         print("Eek!")
+        set_alien_hurt()
     else:
         print("You missed me!")
 
 
 def set_alien_hurt():
-    alien.image = 'alien_hurt'
     sounds.eep.play()
+    alien.image = 'alien_hurt'
     clock.schedule_unique(set_alien_normal, 1.0)
 
 
 def set_alien_normal():
     alien.image = 'alien'
+
+pgzrun.go()
