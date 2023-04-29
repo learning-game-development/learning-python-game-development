370e1b9843259e0b3e1b9603dadb5046a8909de9
Quintin Henn <quintin.henn@gmail.com>
2023-04-29 09:20:37 +0200

    Chapter 8: animation in pygame

---
+++ teach-your-kids-to-code/08-timers-and-animation/SmileyMove.py
@@ -0,0 +1,28 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode([600, 600])
+
+keep_going = True
+pic = pygame.image.load("CrazySmile.bmp")
+
+colorkey = pic.get_at((0, 0))
+pic.set_colorkey(colorkey)
+picx = 0
+picy = 0
+
+BLACK = (0, 0, 0)
+
+while keep_going:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            keep_going = False
+
+    picx += 1  # Move the picture
+    picy += 1
+
+    screen.fill(BLACK)
+    screen.blit(pic, (picx, picy))
+    pygame.display.update()
+
+pygame.quit()
